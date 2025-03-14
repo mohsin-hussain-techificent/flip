4221f4b5471a39ae67b2d252d3247531549b6f24
mohsin-hussain-techificent <mohsin.hussain@techificent.com>
2025-03-15 03:44:05 +0500

    seconf page

---
+++ src/pages/admin.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Admin = () => {
+  return (
+    <div>
+      <h1>Enter Number</h1>
+      <input></input>
+
+      <button>Submit</button>
+    </div>
+  );
+};
+
+export default Admin;
